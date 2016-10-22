17ba517ab4af81bbe426ab0d895c388df24ae710
doc1oo <doc100@gmail.com>
2016-10-22 09:17:47 +0900

    Initial Commit

---
+++ Tb3dScanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tb3dScanner
+//
+//  Created by nanashi on 2016/10/22.
+//  Copyright © 2016年 Shogo Tanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
